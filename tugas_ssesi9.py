05623a889d4739474cb540f6d7668759c06b8a55
hamdan0114 <147163660+hamdan0114@users.noreply.github.com>
2024-05-12 11:48:23 +0700

    Add files via upload

---
+++ tugas_ssesi9.py
@@ -0,0 +1,43 @@
+def tambahkan_teman(teman, nama):
+    teman.append(nama)
+    print("Teman", nama, "telah ditambahkan.")
+
+def hapus_teman(teman, nama):
+    if nama in teman:
+        teman.remove(nama)
+        print("Teman", nama, "telah dihapus.")
+    else:
+        print("Teman", nama, "tidak ditemukan dalam daftar.")
+
+def tampilkan_nama(teman):
+    if teman:
+        print("Daftar Teman:")
+        for nama in teman:
+            print("-", nama)
+    else:
+        print("Daftar teman kosong.")
+
+daftar_teman = []
+
+while True:
+    print("\nMenu:")
+    print("1. Tambahkan Teman")
+    print("2. Hapus Nama")
+    print("3. Tampilkan Nama")
+    print("4. Exit")
+
+    pilihan  = input("pilih menu: ")
+
+    if pilihan == '1':
+        nama_teman = input("Masukkan nama teman yang ingin ditambahkan: ")
+        tambahkan_teman(daftar_teman, nama_teman)
+    elif pilihan == '2':
+        nama_teman = input("Masukkan nama teman yang ingin dihapus: ")
+        hapus_teman(daftar_teman, nama_teman)
+    elif pilihan == '3':
+        tampilkan_nama(daftar_teman)
+    elif pilihan == '4':
+        print("Terima kasih!")
+        break
+    else:
+        print("Menu tidak valid. Silakan pilih kembali.")
